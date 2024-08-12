9c607f82f2dbbbe250bb1cf00e68733c27ffcf34
James Johnson <5540346+jamesjohnson88@users.noreply.github.com>
2024-08-12 22:27:29 +0100

    Added singleton example code

--- Design Patterns/Examples/CSharpDesignPatterns/CSharpDesignPatterns.Singleton/Program.cs
+++ Design Patterns/Examples/CSharpDesignPatterns/CSharpDesignPatterns.Singleton/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("TODO");
+﻿using CSharpDesignPatterns.Singleton.Models;
+
+// Get the single instance of Animal
+var singleAnimalInstance = Animal.Instance;
+
+// Use the methods of the singleton instance
+Console.WriteLine($"The animal species is: {singleAnimalInstance.GetSpecies()}");
